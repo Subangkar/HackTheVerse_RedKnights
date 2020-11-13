ea5d78d9d006fa81c863fa5538393fbf4e17e649
Subangkar <subangkar.karmaker@gmail.com>
2020-11-13 23:57:34 +0600

    avilon homepage template added

--- virtuallab_web/coreapp/views.py
+++ virtuallab_web/coreapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+    """
+    Renders Home Page
+    """
+    template_name = 'coreapp/avilon/student-home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = {'loggedIn': self.request.user.is_authenticated}
+        return ctx
