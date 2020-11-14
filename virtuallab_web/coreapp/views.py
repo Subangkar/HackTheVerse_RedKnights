2700e442dddfbd0a7f5fc8875e1273477c07bee2
Subangkar <subangkar.karmaker@gmail.com>
2020-11-14 16:05:44 +0600

    class pages for student

--- virtuallab_web/coreapp/views.py
+++ virtuallab_web/coreapp/views.py
@@ -102,10 +102,47 @@ def lab(request):
     return render(request, 'coreapp/avilon/student-lab.html')
 
 
-def lab_detail(request):
-    l = [('Simple Pendulum', 'http://www.acs.psu.edu/drussell/Demos/Pendulum/compare-lengths.gif'),
-         ("Young's Experiment", "https://physicsforus.files.wordpress.com/2011/07/doubleslit_animation.gif")] * 6
-    return render(request, 'coreapp/avilon/student-lab-exp.html', context={'exp_list': l})
+def classview(request):
+    try:
+        labname = str(request.GET.get('class')).lower()
+        exp_lists = {
+            'physics': [
+                ('Projectile',
+                 'https://kaiserscience.files.wordpress.com/2015/08/horizontally-launched-projectiles.gif',
+                 reverse('coreapp:experiment', kwargs={'id': 2})),
+                ('Collision', 'https://xmphysics.files.wordpress.com/2019/03/collisiongraphs-a.gif',
+                 reverse('coreapp:experiment', kwargs={'id': 1})),
+                ("Series-Parallel Circuit",
+                 "http://s4.thingpic.com/images/SC/yUMJVwbNDtnrffhX2HHUVH5e.gif"),
+                ("Prism Experiment",
+                 "https://media0.giphy.com/media/l0MYKMrQnwNvLjYhW/source.gif"),
+                ("Young's Experiment",
+                 "https://physicsforus.files.wordpress.com/2011/07/doubleslit_animation.gif"),
+                ('Simple Pendulum', 'http://www.acs.psu.edu/drussell/Demos/Pendulum/compare-lengths.gif'),
+            ],
+            'chemistry': [('NaOH-H2SO4 Titration',
+                           'https://www.buffaloschools.org/cms/lib/NY01913551/Centricity/Domain/6557/titration1.gif'),
+                          ("Bunsen Burner",
+                           "http://classroomclipart.com/images/gallery/Animations/Science/TN_bunsen_burner-4-12_cc.gif"),
+                          ("Ion Test",
+                           "https://pufflesandhoneyadventures.files.wordpress.com/2017/08/metal-ion-flame-test.gif"),
+                          ("Precipitation Test",
+                           "https://pa1.narvii.com/6417/27c8a8e37fa18bcbdf8b8a2f110020ab97d48f0c_hq.gif"),
+                          ],
+            'biology': [('Microscope',
+                         'https://media.giphy.com/media/l41m3Dps5EQ8W1rZS/giphy.gif'),
+                        ("Living Cell Observation",
+                         "http://www.composites.ugent.be/movies/anim_Fatigue_online_microscopy.gif"),
+                        ("Cockroach Dissection",
+                         "http://3.bp.blogspot.com/-9CmeFcEtqIg/Uqbtn6sq6gI/AAAAAAAAAJY/sZXPDJDBzpc/s400/cockroach.gif"),
+                        ("Cell Division",
+                         "https://j.gifs.com/vMEWeX.gif"),
+                        ],
+        }
+        return render(request, 'coreapp/avilon/student-lab-exp.html', context={'exp_list': exp_lists[labname]})
+    except:
+        pass
+    return render(request, 'coreapp/avilon/student-class.html')
 
 
 def chart(request):
